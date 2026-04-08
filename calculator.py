66263080bee497b3b26e08968967117fb97953b1
Smessick123 <smessick@ufl.edu>
2026-04-08 13:22:41 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,36 +1,23 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
 import math
-# First example
-def add(a, b): 
+
+def add(a, b):
     return a + b
 
-def subtract(a, b):
-    return a-b
+def sub(a, b):
+    return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
-    try:
-        c = a / b
-        return c
-    except ZeroDivisionError as e:
-        print(e)
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
 
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid input for logarithm")
+    return math.log(b, a)
 
-def logarithm(a, b):
-    try:
-        c = math.log(b, a)
-        return c
-    except ValueError as e:
-        print(e)
-
-def exponent(a, b):
+def exp(a, b):
     return a ** b
-
-
