22ffc5d998bd4bd942a51e1de0bfff9b49e85f50
DemitriTu <dtu2@ufl.edu>
2026-04-08 20:56:00 -0400

    test_log_invalid_base fix

--- test_calculator.py
+++ test_calculator.py
@@ -65,7 +65,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_log_invalid_base(self): # 1 assertion
         # use same technique from test_divide_by_zero
-        self.assertRaises(ValueError)
+        with self.assertRaises(ValueError):
             logarithm(0,5)
 
 
